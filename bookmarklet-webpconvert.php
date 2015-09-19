f59f9bea6d5339121fcf5b8efbf1e27e1570eca3
n0x5 <coax@audiolabs.org>
2015-09-19 22:06:21 +0200

    Update bookmarklet-webpconvert.php

--- bookmarklet-webpconvert.php
+++ bookmarklet-webpconvert.php
@@ -1,33 +1,53 @@
-<link rel="stylesheet" href="style.css" type="text/css">
 <?php
+
+
+function clean($string) {
+   $string = str_replace(' ', '-', $string); 
+
+    return preg_replace('/[%()]/', '', $string);
+}
+
 $month1 = date("F");
 $month2 = date("Y");
 $month = "$month1$month2";
 $domain = $_GET["i"];
-$homepage = file_get_contents($domain);
 
-$file = basename($domain);
+$encodedUrl = urlencode($domain);
+$domain2 = str_replace(['%2F', '%3A'], ['/', ':'], $encodedUrl);
+
+$homepage = file_get_contents($domain2);
+
+$file = basename($domain2);
+$file33 = clean($file);
 
 $path_parts = pathinfo($file);
-    $pathp2 = $path_parts['filename'];
+    $pathp3 = $path_parts['filename'];
+    $pathp2 = clean($pathp3);
 
 $rmlast = substr($pathp2, 0, -2);
-$folder = "./archive/$month/$rmlast/";
-$maindir = "/your/webp/archive";
+$rmlast2 = urlencode($rmlast);
+$rmlast3 = clean($rmlast2);
+$folder = "./archive/$month/$rmlast3/";
+$maindir = "/home/your/webp/archive";
 
 if (!file_exists("$folder")) {
-    mkdir("$maindir/$month/$rmlast/", 0755, true);
+    mkdir("$maindir/$month/$rmlast3/", 0755, true);
 }
 
-chmod("$maindir/$month/$rmlast/", 0755);
+chmod("$maindir/$month/$rmlast3/", 0755);
 
-file_put_contents($folder . basename($domain), $homepage);
+$domain4 = basename($domain2);
+$domain5 = clean($domain4);
 
-exec ('cwebp -q 80 ' .$maindir.'/'.$month.'/'.$rmlast.'/'.escapeshellarg($file). ' -o '.$maindir.'/'.$month.'/'.$rmlast.'/'.escapeshellarg($pathp2). '.webp');
+file_put_contents($folder . $domain5, $homepage);
 
-exec ('chmod 755 ' .$maindir.'/'.$month.'/'.$rmlast. '/' .escapeshellarg($pathp2). '.webp; rm -rf ' .$maindir.'/' .$month.'/'.$rmlast. '/' .escapeshellarg($file). '');
 
-echo "saved to <a href=\"archive/$month/$rmlast/$pathp2.webp\">$_SERVER[HTTP_HOST]/archive/$month/$pathp2/$pathp2.webp</a><br>";
-echo "<img src=\"archive/$month/$rmlast/$pathp2.webp\" width=\"700px\" < />";
+exec ('cwebp -q 80 ' .$maindir.'/'.$month.'/'.$rmlast3.'/'.escapeshellarg($file33). ' -o '.$maindir.'/'.$month.'/'.$rmlast3.'/'.escapeshellarg($pathp2). '.webp');
+
+exec ('chmod 755 ' .$maindir.'/'.$month.'/'.$rmlast3. '/' .escapeshellarg($pathp2). '.webp; rm -rf ' .$maindir.'/' .$month.'/'.$rmlast3. '/' .escapeshellarg($file33). '');
+
+echo "$domain5";
+echo "saved to <a href=\"archive/$month/$rmlast3/$pathp2.webp\">$_SERVER[HTTP_HOST]/archive/$month/$pathp2/$pathp2.webp</a><br>";
+echo "<img src=\"archive/$month/$rmlast3/$pathp2.webp\" width=\"700px\" < />";
 
 ?>
