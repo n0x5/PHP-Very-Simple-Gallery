a0f7a52451415ea284d67c8d3e8692054faa45d8
n0x5 <coax@audiolabs.org>
2015-09-20 00:15:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,31 @@
 require_once 'head3.php';
 echo '<div id="photos">';
 
+<h2>Latest 6 images...</h2>
+<?php
+
+$month1 = date("F");
+$month2 = date("Y");
+$month = "$month1 $month2";
+$folder = "archive/$month";
+
+$files = glob("/home/your/gallery/$folder/*");
+array_multisort(array_map('filemtime', $files), SORT_DESC, $files);
+$files2 = array_slice($files, 0, 6);
+
+
+foreach($files2 as $file) {
+    $file2 = basename($file);
+    if (basename($file) == "index.html") { continue; }
+      echo "<a href=\"/realtime/$folder/$file2\" target=\"_blank\"><img class=\"latest5img\" src=\"/realtime/$folder/$file2\" width=\"100px\" alt=\"$file2\" title=\"$file2\" /></a>";
+}
+?>
+</div>
+<h2>Archive of months</h2>
+
+
+
+
 $files = glob("archive/*");
 
 array_multisort(
