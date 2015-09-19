b09fd35ad83b110be4f15663940276a2dc8f7ee5
n0x5 <coax@audiolabs.org>
2015-09-19 17:07:21 +0200

    Update bookmarklet-webpconvert.php

--- bookmarklet-webpconvert.php
+++ bookmarklet-webpconvert.php
@@ -14,23 +14,22 @@ $path_parts = pathinfo($file);
 
 $rmlast = substr($pathp2, 0, -2);
 $folder = "./archive/$month/$rmlast/";
+$maindir = "/your/webp/archive";
 
 if (!file_exists("$folder")) {
-    mkdir("/home/user/nrtests/webp/archive/$month/$rmlast/", 0755, true);
+    mkdir("$maindir/$month/$rmlast/", 0755, true);
 }
 
-chmod("/home/user/nrtests/webp/archive/$month/$rmlast/", 0755);
+chmod("$maindir/$month/$rmlast/", 0755);
 
 file_put_contents($folder . basename($domain), $homepage);
 
-exec ('cwebp -q 80 /home/user/nrtests/webp/archive/' .$month.'/'.$rmlast.'/'.escapeshellarg($file). ' -o /home/user/nrtests/webp/archive/'.$month.'/'.$rmlast.'/'.escapeshellarg($pathp2). '.webp');
+exec ('cwebp -q 80 ' .$maindir.'/'.$month.'/'.$rmlast.'/'.escapeshellarg($file). ' -o '.$maindir.'/'.$month.'/'.$rmlast.'/'.escapeshellarg($pathp2). '.webp');
 
+exec ('chmod 755 ' .$maindir.'/'.$month.'/'.$rmlast. '/' .escapeshellarg($pathp2). '.webp; rm -rf ' .$maindir.'/' .$month.'/'.$rmlast. '/' .escapeshellarg($file). '');
 
-exec ('chmod 755 /home/user/nrtests/webp/archive/' .$month.'/' .$rmlast. '/' .escapeshellarg($pathp2). '.webp; rm -rf /home/user/nrtests/webp/archive/' .$month.'/' .$rmlast. '/' .escapeshellarg($file). '');
-
-echo "<br>";
-echo "saved to <a href=\"archive/$month/$rmlast/$pathp2.webp\">$_SERVER[HTTP_HOST]/archive/$month/$pathp2/$pathp2.webp</a>";
-echo "<img src=\"archive/$month/$rmlast/$pathp2.webp\" width=\"700px\" < /><br>";
+echo "saved to <a href=\"archive/$month/$rmlast/$pathp2.webp\">$_SERVER[HTTP_HOST]/archive/$month/$pathp2/$pathp2.webp</a><br>";
+echo "<img src=\"archive/$month/$rmlast/$pathp2.webp\" width=\"700px\" < />";
 
 ?>
 
