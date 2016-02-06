1ca087e3cd7f458f002f8cd377e16c20ead47570
n0x5 <coax@audiolabs.org>
2016-02-06 16:45:00 +0100

    Update archcon2jpg.php

--- archcon2jpg.php
+++ archcon2jpg.php
@@ -1,9 +1,15 @@
 <?php
+//define('WP_USE_THEMES', true);
+//require('/home/wp/wp-blog-header.php');
+?>
+
+
+<?php
 require_once 'head3.php';
 echo '<div id="photos">';
 $domain = $_GET["which"];
-$domain2 = htmlspecialchars($domain);
-$files = glob("archive/$domain2/*");
+$domain2 = rawurlencode($domain);
+$files = glob("archive/$domain/*");
 
 array_multisort(
 array_map( 'filemtime', $files ),
@@ -17,11 +23,12 @@ echo "<title>$domain $count images</title>";
 echo "<p>Gallery: $domain2, items: $count</p>";
 foreach($files as $file) {
     $file2 = rawurlencode(basename($file));
-    $path_parts = pathinfo($file);
-    $pathp2 = $path_parts['filename'];
+    $path_parts = rawurlencode(pathinfo($file));
+    $pathp2 = rawurlencode($path_parts['filename']);
     $ftime = date ("F d Y H:i:s", filemtime($file));
     if (basename($file) == "index.html") { continue; }
-      echo "<div class=\"imgbox\"><div class=\"ftime\">$ftime</div><a href=\"archive/$domain/$file2\" target=\"_blank\"><img src=\"phpThumb.php?src=/archive/$domain/$pathp21/$file2&amp;w=250&amp;h=250&amp;far=C&amp;bg=ffffff\" height=\"250\" alt=\"$file2\" title=\"$file2\"></a></div>";
+      list($width, $height) = getimagesize("$file");
+      echo "<div class=\"imgbox\"><div class=\"ftime\">$ftime</div><a href=\"archive/$domain/$file2\" target=\"_blank\"><img src=\"phpThumb.php?src=/archive/$domain2/$pathp22/$file2&amp;w=250&amp;h=250&amp;far=C&amp;bg=ffffff\" height=\"250\" alt=\"$file2\" title=\"$file2\"></a><div class=\"dimension2\">$width x $height</div> </div>";
 
 }
 ?>
